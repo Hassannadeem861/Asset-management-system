8d91919f4e837774559f5b9e4d1033b942533837
Hassan Nadeem <125460447+Hassannadeem861@users.noreply.github.com>
2025-04-29 02:56:17 -0700

    modified

--- backend/server.js
+++ backend/server.js
@@ -66,9 +66,9 @@ app.get("/", (req, res) => {
 });
 
 // Set port and listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
 
 export default app;
